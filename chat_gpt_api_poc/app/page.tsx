ff5b60c77c77fd00ab41a6e246793ea2dfff48fc
KevinGage <dudeman02379@gmail.com>
2023-04-07 12:54:21 -0400

    started some very basic styling

--- chat_gpt_api_poc/app/page.tsx
+++ chat_gpt_api_poc/app/page.tsx
@@ -63,48 +63,66 @@ export default function Home() {
   return (
     <main>
       <h1>Chat</h1>
-      <form>
-        <h2>Settings</h2>
-        <label htmlFor="temperature">
-          Temperature (Higher gets more random results)
-        </label>
-        <input
-          type="range"
-          id="temperature"
-          name="temperature"
-          value={temperature}
-          onChange={(e) => setTemperature(parseInt(e.target.value))}
-          min={0}
-          max={20}
-          step={1}
-        />
-        <label htmlFor="behaviour">Assistant behaviour</label>
-        <input
-          type="text"
-          id="behaviour"
-          name="behaviour"
-          placeholder="Assistant behaviour"
-          value={assistantBehaviour}
-          onChange={(e) => setAssistantBehaviour(e.target.value)}
-        />
-        <br />
-        <input
-          type="text"
-          id="prompt"
-          name="prompt"
-          placeholder="Type your chat message here"
-          value={prompt}
-          onChange={(e) => setPrompt(e.target.value)}
-        />
-      </form>
-      <ChatPane messages={messageHistory} />
-      {isLoading && <Waiting />}
-      <button onClick={chat}>Send</button>
-      <button onClick={clearHistory}>Clear</button>
-      <h2>Cost</h2>
-      <p>Tokens: {chatTokens}</p>
-      {/* gpt-3.5-turbo	$0.002 / 1K tokens */}
-      <p>${(chatTokens * 0.002) / 1000}</p>
+      <div className="p-2">
+        <form>
+          <h2>Settings</h2>
+          <div>
+            <label htmlFor="temperature">
+              Temperature (Higher gets more random results)
+            </label>
+            <input
+              type="range"
+              id="temperature"
+              name="temperature"
+              value={temperature}
+              onChange={(e) => setTemperature(parseInt(e.target.value))}
+              min={0}
+              max={20}
+              step={1}
+            />
+          </div>
+          <div>
+            <label htmlFor="behaviour">Assistant behaviour</label>
+            <input
+              type="text"
+              id="behaviour"
+              name="behaviour"
+              placeholder="Assistant behaviour"
+              value={assistantBehaviour}
+              onChange={(e) => setAssistantBehaviour(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="prompt">Prompt</label>
+            <input
+              type="text"
+              id="prompt"
+              name="prompt"
+              placeholder="Type your chat message here"
+              value={prompt}
+              onChange={(e) => setPrompt(e.target.value)}
+            />
+          </div>
+        </form>
+        <ChatPane messages={messageHistory} />
+        {isLoading && <Waiting />}
+        <button
+          className="text-white bg-blue-700 hover:bg-blue-800 rounded-lg p-2 m-2 dark:bg-blue-600 dark:hover:bg-blue-700"
+          onClick={chat}
+        >
+          Send
+        </button>
+        <button
+          className="text-white bg-red-700 hover:bg-red-800 rounded-lg p-2 m-2 dark:bg-red-600 dark:hover:bg-red-700"
+          onClick={clearHistory}
+        >
+          Clear
+        </button>
+        <h2>Chat Total Cost</h2>
+        <p>Tokens: {chatTokens}</p>
+        {/* gpt-3.5-turbo	$0.002 / 1K tokens */}
+        <p>${(chatTokens * 0.002) / 1000}</p>
+      </div>
     </main>
   );
 }
