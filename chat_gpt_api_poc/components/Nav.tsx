799daf5ece5ed62e6f27f48f1362496d0adcf479
KevinGage <dudeman02379@gmail.com>
2023-04-07 21:33:27 -0400

    styled nav

--- chat_gpt_api_poc/components/Nav.tsx
+++ chat_gpt_api_poc/components/Nav.tsx
@@ -1,12 +1,26 @@
+"use client";
+
 import Link from "next/link";
+import { useSelectedLayoutSegment } from "next/navigation";
 
 export default function Nav() {
+  const segment = useSelectedLayoutSegment();
+
   return (
     <nav>
-      <ul>
-        <li>
-          <Link href="/">Chat</Link>
-          <Link href="/images">Images</Link>
+      <ul className="flex px-5 py-2 gap-2 w-full items-center text-xl">
+        <li className="hover:bg-slate-900 px-5 py-2 rounded-md">
+          <Link href="/" className={segment === null ? " underline" : ""}>
+            Chat
+          </Link>
+        </li>
+        <li className="hover:bg-slate-900 px-5 py-2 rounded-md">
+          <Link
+            href="/images"
+            className={segment === "images" ? "underline" : ""}
+          >
+            Images
+          </Link>
         </li>
       </ul>
     </nav>
